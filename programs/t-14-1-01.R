0d86de8fa3b7bcae1fd9de4448e1c69f369794c1
Ross Sharp <ross.sharp@dominodatalab.com>
2022-05-18 14:38:16 -0700

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/ross_sharp/Clinical_Study_1?executionId=628560d14906b439238b955c

--- programs/t-14-1-01.R
+++ programs/t-14-1-01.R
@@ -8,8 +8,8 @@ library(assertthat)
 library(huxtable)
 library(pharmaRTF)
 
-source('./mnt/code/config.R')
-source('./mnt/code/funcs.R')
+source('/mnt/code/programs/config.R')
+source('/mnt/code/programs/funcs.R')
 
 # Read in the ADLB datasets
 adsl <- read_xpt(glue("/mnt/data/Clincal_Study_1_ADaM/adsl.xpt"))
@@ -75,7 +75,7 @@ ht <- as_hux(final, add_colnames = FALSE) %>%
 
 # Write into doc object and pull titles/footnotes from excel file
 doc <- rtf_doc(ht) %>% titles_and_footnotes_from_df(
-  from.file='./mnt/code/data/titles.xlsx',
+  from.file='/mnt/code/data/titles.xlsx',
   reader=example_custom_reader,
   table_number='14-1.01') %>%
   set_font_size(10) %>%
@@ -83,4 +83,4 @@ doc <- rtf_doc(ht) %>% titles_and_footnotes_from_df(
   set_column_header_buffer(top=1)
 
 # Write out the RTF
-write_rtf(doc, file='./mnt/artifacts/outputs/14-1.01.rtf')
+write_rtf(doc, file='/mnt/artifacts/outputs/14-1.01.rtf')
